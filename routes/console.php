f77799dc63b730fdac3a5aa4fd0b7020f980b4dc
Himanshu Ramavat <77565273+himanshuramavat@users.noreply.github.com>
2024-11-20 17:23:51 +0000

    [TASK] check lints

--- routes/console.php
+++ routes/console.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Artisan;
 */
 
 Artisan::command('inspire', function () {
+    /** @var \Illuminate\Console\Command $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
