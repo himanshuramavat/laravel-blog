a144ae88ff4ea4c5f5c4da5d420d30205c3e859f
Himanshu Ramavat <77565273+himanshuramavat@users.noreply.github.com>
2024-11-21 16:27:49 +0000

    [TASK] CI Test

--- routes/console.php
+++ routes/console.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Artisan;
 */
 
 Artisan::command('inspire', function () {
+    //@phpstan-ignore-line
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
